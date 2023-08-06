fd75b5897facefa50a99e1f1c0db9c49c8e2a3ca
Jose Rankis <141494710+joserkis@users.noreply.github.com>
2023-08-07 01:17:19 +0300

    packages added

---
+++ Crypto_Wallets_Scanner/Main.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Crypto_Wallets_Scanner
+{
+    public partial class Main : Form
+    {
+        private BackgroundWorker worker;
+
+        public Main()
+        {
+            InitializeComponent();
+
+            // Initialize the BackgroundWorker
+            worker = new BackgroundWorker();
+            worker.WorkerReportsProgress = true;
+            worker.DoWork += Worker_DoWork;
+            worker.ProgressChanged += Worker_ProgressChanged;
+            worker.RunWorkerCompleted += Worker_RunWorkerCompleted;
+        }
+
+        List<string> wallets = new List<string>();
+
+        private void button1_MouseHover(object sender, EventArgs e)
+        {
+            start.ForeColor = Color.White;
+        }
+
+        private void button1_MouseLeave(object sender, EventArgs e)
+        {
+            start.ForeColor = Color.FromArgb(234, 22, 70);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            result_txt.Clear();
+            if (!worker.IsBusy)
+            {
+                // Clear the result textbox before starting the background task
+                result_txt.Text = string.Empty;
+
+                // Start the background task
+                worker.RunWorkerAsync();
+            }
+        }
+
+        private void Worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            // Perform the background task here
+            List<string> blockchains = new List<string>
+            {
+                "https://bscscan.com/address/",
+                "https://etherscan.io/address/",
+                "https://polygonscan.com/address/",
+                "https://ftmscan.com/address/",
+                "https://snowtrace.io/address/",
+                "https://moonriver.moonscan.io/",
+                "https://moonscan.io/address/",
+                "https://cronoscan.com/address/"
+            };
+
+            Wallets_Scanner ws = new Wallets_Scanner();
+
+            Invoke((Action)(() =>
+            {
+                wallets.Clear();
+                foreach (string item in wallets_txt.Lines)
+                {
+                    wallets.Add(item);
+                }
+                label4.Text = wallets.Count.ToString();
+            }));
+
+            int totalWallets = wallets.Count * blockchains.Count;
+            int progress = 0;
+
+            foreach (string wallet in wallets)
+            {
+                for (int b = 0; b < blockchains.Count; b++)
+                {
+                    string blockch;
+                    if (b == 0)
+                    {
+                        blockch = "BNB";
+                    }
+                    else if (b == 1)
+                    {
+                        blockch = "Ether";
+                    }
+                    else if (b == 2)
+                    {
+                        blockch = "Polygon";
+                    }
+                    else if (b == 3)
+                    {
+                        blockch = "Fantom";
+                    }
+                    else if (b == 4)
+                    {
+                        blockch = "Avalanche";
+                    }
+                    else if (b == 5)
+                    {
+                        blockch = "MoonRiver";
+                    }
+                    else if (b == 6)
+                    {
+                        blockch = "MoonBeam";
+                    }
+                    else
+                    {
+                        blockch = "Cronos";
+                    }
+
+                    string result = ws.GetWalletBallance(wallet, blockchains[b], blockch);
+
+                    // Report progress and result to the UI thread
+                    worker.ReportProgress((progress * 100) / totalWallets, result);
+
+                    progress++;
+                }
+            }
+        }
+
+        private void Worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            // Update the UI with the progress and result
+            result_txt.AppendText(e.UserState.ToString());
+        }
+
+        private void Worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            // The background task is completed
+        }
+
+        private void start_MouseEnter(object sender, EventArgs e)
+        {
+            start.ForeColor = Color.White;
+        }
+
+        private void richTextBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Handle key press event if needed
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,30 @@
+
+# Crypto Wallets Scanner
+
+This tool can scan list of wallets addresses on 8 Crypto Networks with one click
+
+
+## Screenshots
+
+![App Screenshot](https://github.com/AbdeLhalimSB/Crypto-Wallets-Scanner/blob/main/Crypto_Wallets_Scanner/Resources/Capture.PNG)
+
+
+## Usage
+
+```javascript
+Download the Portable executable file of Stable Version from releases
+When you click on start the tool look like cracked but it not :)
+```
+
+
+
+## Crypto Networks Supported
+
+- Binance Smart Chain
+- Ethereum
+- Polygon
+- Fantom
+- Avalanche
+- MoonRiver
+- MoonBeam
+- Cronos
